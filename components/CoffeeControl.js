2114ba2f0fc326be484158aab7b5d7b4b4cbc60b
David Jandron <jandron1211@gmail.com>
2023-04-21 10:02:16 -0700

    ADD code to CoffeeControl

---
+++ components/CoffeeControl.js
@@ -0,0 +1,110 @@
+import React from "react";
+import NewCoffeeForm from "./NewCoffeeForm";
+import CoffeeDetail from "./CoffeeDetails";
+import CoffeeList from "./CoffeeList";
+import EditCoffeeForm from "./EditCoffeeForm";
+
+class CoffeeControl extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      pageView: false,
+      mainCoffeeList: [],
+      selectedCoffee: null,
+      editing: false
+    }
+  }
+
+  handleClick = () => {
+    if(this.state.selectedCoffee !== null) {
+      this.setState({
+        pageView: false,
+        selectedCoffee: null,
+        editing: false
+      });
+    } else {
+      this.setState(prevState => ({
+        pageView: !prevState.pageView
+      }));
+    }
+  }
+
+  handleEditClick = () => {
+    this.setState({editing: true});
+  }
+
+  handleAddingNewCoffeeToList = (newCoffee) => {
+    const newMainCoffeeList = this.state.mainCoffeeList.concat(newCoffee);
+    this.setState({mainCoffeeList: newMainCoffee, pageView: false})
+  }
+
+  handleChangingselectedCoffee = (id) => {
+    const selectedCoffee = this.state.mainCoffeeList.filter(coffee => coffee.id === id)[0];
+    this.setState({selectedCoffee: selectedCoffee});
+  }
+
+  handleDeletingCoffee = (id) => {
+    const newMainCoffeeList = this.state.mainCoffeeList.filter(coffee => coffee.id !== id);
+    this.setState({
+      mainCoffeeList: newMainCoffeeList,
+      selectedCoffee: null
+    });
+  }
+
+  handleEditingCoffeeInList = (coffeeToEdit) => {
+    const editedMainCoffeeList = this.state.mainCoffeeList.filter(coffee => coffee.id !== this.state.selectedCoffee.id).concat(coffeeToEdit);
+    this.setState({
+      mainCoffeeList: editedMainCoffeeList,
+      editing: false, 
+      selectedCoffee: null
+    });
+  }
+
+  handleSellCoffee = (coffeeToSell) => {
+    if(coffeeToSell.quantity <= 0){
+      let newQuantity = { ...coffeeToSell, quantity: 0 }
+      const soldMainCoffeeList = this.state.mainCoffeeList.filter(coffee => coffee.id !== this.state.selectedCoffee.id).concat(newQuantity);
+      this.setState({
+        mainCoffeeList: soldMainCoffeeList,
+        selectedCoffee: null
+      })
+    } else {
+      let number = parseInt(coffeeToSell.quantity);
+      let newQuantity = { ...coffeeToSell, quantity: (number - 1) }
+      const soldMainCoffeeList = this.state.mainCoffeeList.filter(coffee => coffee.id !== this.state.selectedCoffee.id).concat(newQuantity);
+      this.setState({
+        mainCoffeeList: soldMainCoffeeList,
+        selectedCoffee: null
+      });
+    }
+  }
+
+  render() {
+    let currentPage = null;
+    let buttonText = null;
+
+    if(this.state.editing === true) {
+      currentPage = <EditCoffeeForm coffeeObject={this.state.selectedCoffee} onEditCoffee = {this.handleEditingCoffeeInList}/>
+      buttonText="Return to List";
+    } else if (this.state.selectedCoffee !== null) {
+      currentPage = <CoffeeDetail coffeeObject = {this.state.selectedCoffee} onClickingDelete={this.handleDeletingCoffee} onClickingEdit = {this.handleEditClick} sell={this.handleSellCoffee}/>
+      buttonText="Return to List"
+    } else if (this.state.pageView) {
+      currentPage = <NewCoffeeForm onNewCoffeeCreation = {this.handleAddingNewCoffeeToList}/>
+      buttonText="Return to List"
+    } else {
+      currentPage = <CoffeeList coffeeList = {this.state.mainCoffeeList} onCoffeeSelection = {this.handleChangingselectedCoffee}/>
+      buttonText = "Buy Coffee"
+    }
+    return(
+      <React.Fragment>
+        {currentPage}
+        <button onClick={this.handleClick}>{buttonText}</button>
+      </React.Fragment>
+    );
+  }
+
+}
+
+export default CoffeeControl;
---
+++ components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <React.Fragment>
+      <h1>Get Coffee</h1>
+    </React.Fragment>
+  );
+}
+
+export default Header;
