00f6c9aa1bd5db60a355955185597b5c5492afc4
David Jandron <jandron1211@gmail.com>
2023-04-21 16:10:08 -0700

    ADD CSS styling zig-zags

--- src/components/App.js
+++ src/components/App.js
@@ -4,11 +4,14 @@ import CoffeeControl from "./CoffeeControl"
 
 function App(){
   const myStyledComponentStyles = {
+    background: 'linear-gradient(63deg, #999 23%, transparent 23%) 7px 0, linear-gradient(63deg, transparent 74%, #999 78%), linear-gradient(63deg, transparent 34%, #999 38%, #999 58%, transparent 62%), #444',
+    backgroundSize: '16px 48px',
     backgroundColor: '#2C0D06',
     color: '#DFFF00',
     fontFamily: 'sans-serif',
-    paddingTop: '60px',
-    paddingBottom: '60px',
+    paddingTop: '45px',
+    paddingBottom: '100px',
+    textAlign: 'center',
   }
   return (
     <React.Fragment>
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,10 +2,11 @@ import React from "react";
 import CoffeeBags from '../img/CoffeeBags.jpg';
 
 function Header() {
+
   return (
     <React.Fragment>
-      <h1 style={{textAlign: "center"}}>Why Not Coffee?</h1>
-      <center><img src={CoffeeBags} style= {{ width: 350, height: 350, paddingBottom: '15px' }} alt='burlap bags of coffee' /></center>
+        <h1 style={{ textAlign: "center", fontSize: "50px" }}>Why Not Coffee?</h1>
+        <center><img src={CoffeeBags} style= {{ width: 350, height: 350, paddingBottom: '10px' }} alt='burlap bags of coffee' /></center>
     </React.Fragment>
   );
 }
