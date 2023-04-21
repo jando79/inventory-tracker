7e851b2794fdfb336426130144719f891b46fe86
David Jandron <jandron1211@gmail.com>
2023-04-21 12:13:01 -0700

    ADD EditCoffeeForm

---
+++ src/components/EditCoffeeForm.js
@@ -0,0 +1,34 @@
+import React from "react";
+import ReusableForm from "./ReusableForm";
+import PropTypes from "prop-types";
+
+function EditCoffeeForm (props) {
+  const { coffeeObject } = props;
+
+  function handleEditCoffeeFormSubmission(event) {
+    event.preventDefault();
+    props.onEditCoffee({
+      coffee: event.target.coffee.value,
+      origin: event.target.origin.value,
+      roast: event.target.roast.value,
+      price: event.target.price.value,
+      quantity: wineObject.quantity,
+      id: coffeeObject.id
+    })
+  }
+
+  return (
+    <React.Fragment>
+      <ReusableForm 
+        formSubmissionHandler={handleEditCoffeeFormSubmission}
+        buttonText="Update Coffee" />
+    </React.Fragment>
+  );
+}
+
+EditCoffeeForm.propTypes = {
+  coffeeObject: PropTypes.object,
+  onEditCoffee: PropTypes.func
+}
+
+export default EditCoffeeForm;
