dc726bf05818515b99f6c72ae7afeec7cd2abf15
David Jandron <jandron1211@gmail.com>
2023-04-21 15:22:59 -0700

    WIP CSS styling

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,20 @@ import React from "react";
 import Header from "./Header";
 import CoffeeControl from "./CoffeeControl"
 
-function App() {
+function App(){
+  const myStyledComponentStyles = {
+    backgroundColor: '#2C0D06',
+    color: '#DFFF00',
+    fontFamily: 'sans-serif',
+    paddingTop: '60px',
+    paddingBottom: '60px',
+  }
   return (
     <React.Fragment>
-      <Header />
-      <CoffeeControl />
+      <div style={myStyledComponentStyles}>
+        <Header />
+        <CoffeeControl />
+      </div>
     </React.Fragment>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ function Header() {
   return (
     <React.Fragment>
       <h1 style={{textAlign: "center"}}>Why Not Coffee?</h1>
-      <img src={CoffeeBags} alt='burlap bags of coffee' />
+      <center><img src={CoffeeBags} style= {{ width: 350, height: 350, paddingBottom: '15px' }} alt='burlap bags of coffee' /></center>
     </React.Fragment>
   );
 }
