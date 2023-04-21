59bdfb9d32db0c10a5c69c6888f866c8f5e25a0f
David Jandron <jandron1211@gmail.com>
2023-04-21 14:52:13 -0700

    ADD img

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,11 @@
 import React from "react";
+import CoffeeBags from '../img/CoffeeBags.jpg';
 
 function Header() {
   return (
     <React.Fragment>
       <h1 style={{textAlign: "center"}}>Why Not Coffee?</h1>
+      <img src={CoffeeBags} alt='burlap bags of coffee' />
     </React.Fragment>
   );
 }
